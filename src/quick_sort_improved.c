61ba14874a2b7e997a2ff2b1f2113fc3a745ce35
Susuta Ryosuke <susut_t1999@icloud.com>
2019-06-25 15:51:56 +0900

    comment

--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -18,6 +18,26 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+    int i,j,pivot,same;
+    pivot = A[n/2];
+    A[n/2] = A[0];
+    A[0] = pivot;
+    same = 0;
+    for (i = j = 1; i < n; i++){
+        if (A[i] < pivot){
+            swap(A + i, A + j);
+            j++;
+        }
+        else if(A[i] == pivot){
+            swap(A+i,A+j);
+            same++;
+            swap(A+same,A+i);
+            j++;
+        }
+    }
+    swap(A,A+j-1); 
+    if(j-1>1) quick_sort(A,j-1);
+    if(n-j >1)quick_sort(A+j+same,n-j-same);
 }
 
 int main(){
